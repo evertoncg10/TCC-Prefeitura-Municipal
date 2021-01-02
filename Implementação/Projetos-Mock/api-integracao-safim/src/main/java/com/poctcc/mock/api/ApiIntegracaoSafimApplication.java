532d27a3e1a4fd4a37a88001a0c948657cac0362
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-01-02 17:32:41 -0300

    Criação do projeto de integração com o SAFIM.

---
+++ Implementação/Projetos-Mock/api-integracao-safim/src/main/java/com/poctcc/mock/api/ApiIntegracaoSafimApplication.java
@@ -0,0 +1,13 @@
+package com.poctcc.mock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIntegracaoSafimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIntegracaoSafimApplication.class, args);
+	}
+
+}
