56dfd9ad4a19704fbc7f465c0a4c873fb3f07947
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-01-13 17:45:48 -0300

    Ajustes enumerador de Sexo.

--- Implementação/Projetos-Mock/api-integracao-safim/src/main/java/com/poctcc/mock/api/enums/Sexo.java
+++ Implementação/Projetos-Mock/api-integracao-safim/src/main/java/com/poctcc/mock/api/enums/Sexo.java
@@ -2,15 +2,35 @@ package com.poctcc.mock.api.enums;
 
 public enum Sexo {
 
-	MASCULINO('M'), FEMININO('F');
+	MASCULINO('M', "Masculino"), FEMININO('F', "Feminino");
 
-	private final char valor;
+	private final char sigla;
+	private final String descricao;
 
-	Sexo(char valorSexo) {
-		valor = valorSexo;
+	Sexo(char sigla, String descricao) {
+		this.sigla = sigla;
+		this.descricao = descricao;
 	}
 
-	public char getValor() {
-		return valor;
+	public char getSigla() {
+		return sigla;
 	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public boolean sameOf(char sigla) {
+		return String.valueOf(this.sigla).equalsIgnoreCase(String.valueOf(sigla));
+	}
+
+	public static Sexo of(char sigla) {
+		for (Sexo sexo : values()) {
+			if (sexo.sameOf(sigla)) {
+				return sexo;
+			}
+		}
+		return null;
+	}
+
 }
