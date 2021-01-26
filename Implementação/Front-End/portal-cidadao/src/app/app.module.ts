c5d9595aa7ea34ce61e464634b47f35d31bedc44
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-01-25 22:10:30 -0300

    Criação da Home e ajustes.

--- Implementação/Front-End/portal-cidadao/src/app/app.module.ts
+++ Implementação/Front-End/portal-cidadao/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './views/template/header/header.component';
 import { FooterComponent } from './views/template/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
