798b56691a7df029b29e142110254b5af40203e1
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-01-31 20:55:38 -0300

    Implementação página COVID 19.

--- Implementação/Front-End/portal-cidadao/src/app/app-routing.module.ts
+++ Implementação/Front-End/portal-cidadao/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ACidadeComponent } from './views/a-cidade/a-cidade.component';
+import { Covid19Component } from './views/covid19/covid19.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { ConsultaImoveisComponent } from './views/portal/consulta-imoveis/consulta-imoveis.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'a-cidade', component: ACidadeComponent},
+  {path: 'covid-19', component: Covid19Component},
   {path: 'portal/home', component: HomePortalComponent},
   {path: 'portal/imoveis', component: ConsultaImoveisComponent}
 ];
--- Implementação/Front-End/portal-cidadao/src/app/app.module.ts
+++ Implementação/Front-End/portal-cidadao/src/app/app.module.ts
@@ -6,19 +6,20 @@ import { AppComponent } from './app.component';
 import { BannerHomeComponent } from './components/banner-home/banner-home.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
 import { NoticiasComponent } from './components/noticias/noticias.component';
-import { ServicosHomePortalComponent } from './components/servicos-home-portal/servicos-home-portal.component';
-import { HomeComponent } from './views/home/home.component';
-import { LoginComponent } from './views/login/login.component';
-import { HomePortalComponent } from './views/portal/home-portal/home-portal.component';
-import { FooterComponent } from './views/template/footer/footer.component';
-import { HeaderComponent } from './views/template/header/header.component';
-import { ConsultaImoveisComponent } from './views/portal/consulta-imoveis/consulta-imoveis.component';
+import { PainelImoveisComponent } from './components/painel-imoveis/painel-imoveis.component';
 import { PainelPessoaFisicaComponent } from './components/painel-pessoa-fisica/painel-pessoa-fisica.component';
 import { PainelPessoaJuridicaComponent } from './components/painel-pessoa-juridica/painel-pessoa-juridica.component';
-import { TituloComponent } from './views/template/titulo/titulo.component';
-import { PainelImoveisComponent } from './components/painel-imoveis/painel-imoveis.component';
-import { PortalHeaderComponent } from './views/portal/portal-header/portal-header.component';
+import { ServicosHomePortalComponent } from './components/servicos-home-portal/servicos-home-portal.component';
 import { ACidadeComponent } from './views/a-cidade/a-cidade.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ConsultaImoveisComponent } from './views/portal/consulta-imoveis/consulta-imoveis.component';
+import { HomePortalComponent } from './views/portal/home-portal/home-portal.component';
+import { PortalHeaderComponent } from './views/portal/portal-header/portal-header.component';
+import { FooterComponent } from './views/template/footer/footer.component';
+import { HeaderComponent } from './views/template/header/header.component';
+import { TituloComponent } from './views/template/titulo/titulo.component';
+import { Covid19Component } from './views/covid19/covid19.component';
 
 @NgModule({
   declarations: [
@@ -39,6 +40,7 @@ import { ACidadeComponent } from './views/a-cidade/a-cidade.component';
     PainelImoveisComponent,
     PortalHeaderComponent,
     ACidadeComponent,
+    Covid19Component
   ],
   imports: [
     BrowserModule,
