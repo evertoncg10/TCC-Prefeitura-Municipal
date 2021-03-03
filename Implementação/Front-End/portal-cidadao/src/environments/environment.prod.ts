abbd217edbc2bb35c5bcfd0449a0c282b37ebbdd
Jeferson Ribeiro dos Santos <jeferson.santos@amcom.com.br>
2021-03-02 22:41:52 -0300

    Ajustes para publicação

--- Implementação/Front-End/portal-cidadao/src/environments/environment.prod.ts
+++ Implementação/Front-End/portal-cidadao/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,  
-  apiAuth: 'http://34.121.29.175:8081/v1/api/auth/signin',
-  apiCidadao: 'http://app-cidadao:8080/pessoas',
-  apiImovel: 'http://app-imovel:8060/pessoas'
+  apiAuth: 'http://localhost:8081/v1/api/auth/signin',
+  apiCidadao: 'http://localhost:8080/pessoas',
+  apiImovel: 'http://localhost:8060/pessoas'
 };
