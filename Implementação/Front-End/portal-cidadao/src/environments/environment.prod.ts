c8970965ff8d98c8cc315724ea68aabfffe43748
Jeferson Ribeiro dos Santos <jeferson.santos@amcom.com.br>
2021-03-03 00:52:22 -0300

    Ajustes interceptor e autenticação

--- Implementação/Front-End/portal-cidadao/src/environments/environment.prod.ts
+++ Implementação/Front-End/portal-cidadao/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,  
-  apiAuth: 'http://10.128.0.21:8081/v1/api/auth/signin',
-  apiCidadao: 'http://10.128.0.21:8080/pessoas',
-  apiImovel: 'http://10.128.0.21:8060/pessoas'
+  apiAuth: 'http://34.69.154.232:8081/v1/api/auth/signin',
+  apiCidadao: 'http://34.69.154.232:8080/pessoas',
+  apiImovel: 'http://34.69.154.232:8060/pessoas'
 };
