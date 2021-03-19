e01875ef1e7d7a0a25758d9a50064d39bf242ea8
Jeferson Ribeiro dos Santos <jeferson.santos@amcom.com.br>
2021-03-18 23:03:09 -0300

    Implementando segurança mínima para testes

--- Implementação/Projetos-Mock/api-coleta-lixo/src/main/java/br/com/api/coletalixo/controller/ColetaController.java
+++ Implementação/Projetos-Mock/api-coleta-lixo/src/main/java/br/com/api/coletalixo/controller/ColetaController.java
@@ -1,13 +1,13 @@
 package br.com.api.coletalixo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.api.coletalixo.dto.ColetaDTO;
 import br.com.api.coletalixo.service.IColetaService;
 
 @RestController
@@ -18,8 +18,11 @@ public class ColetaController {
 	private IColetaService service;
 	
 	@GetMapping
-	public List<ColetaDTO> listarTodos(){
-		return service.listarTodos();
+	public ResponseEntity<?> listarTodos(@RequestParam String email, @RequestParam String token){
+		return !"teste.implementacao.pucmg@gmail.com".equals(email) 
+				|| !"8e776bf44b319ccc60f981e3288938744110b0370f24fef75fb3413f77a93699".equals(token) ?
+				ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access") :
+				ResponseEntity.ok(service.listarTodos());
 	}
 
 }
